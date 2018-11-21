ba70b9f525a6f404705598740ae75b8ddac61649
Greg Morgan <gregm9999@gmail.com>
2018-11-21 23:59:53 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -83,7 +83,7 @@ class Student
     sql = <<-SQL
       SELECT * FROM students WHERE grade = 10
     SQL
-    DB[:conn].execute(sql).collect do |row|
+    DB[:conn].execute(sql).map do |row|
       self.new_from_db(row)
     end.first
   end
