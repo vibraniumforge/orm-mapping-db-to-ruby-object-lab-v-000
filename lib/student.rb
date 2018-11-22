c812a9402e3491189f007241b631578c5a4eb03b
Greg Morgan <gregm9999@gmail.com>
2018-11-22 00:03:33 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -54,7 +54,7 @@ class Student
 
   def self.students_below_12th_grade
     sql = <<-SQL
-      SELECT * FROM students WHERE grade <=11
+      SELECT * FROM students WHERE grade <12
     SQL
     DB[:conn].execute(sql).map do |row|
       self.new_from_db(row)
